5d3e9db63ad74a17eb56d98777e027896cea8c45
Abhishek Amar Singh <abhisheksingh.120120@gmail.com>
2022-01-22 21:58:41 +0530

    first commit

---
+++ C2TC_M05/src/ArgsDemo.java
@@ -0,0 +1,18 @@
+public class ArgsDemo
+{
+	public static void main(String[] args)
+	{
+		System.out.println(args.toString());
+		if(args.length==0)
+		{
+			System.out.println("length of args[] is 0.");
+		}
+		else
+		{
+			for(String i:args)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
