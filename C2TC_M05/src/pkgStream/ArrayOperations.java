e8c78d77478e7d90208dbf1104324eb081abbe25
Abhishek Amar Singh <abhisheksingh.120120@gmail.com>
2022-04-16 17:03:57 +0530

    first commit

--- C2TC_M05/src/pkgStream/ArrayOperations.java
+++ C2TC_M05/src/pkgStream/ArrayOperations.java
@@ -20,5 +20,13 @@ public class ArrayOperations
 		int max = Arrays.stream(numbers).max().getAsInt();
 		int min = Arrays.stream(numbers).min().getAsInt();
 		System.out.println("\nmax=" + max + " min=" + min);
+		
+		//find sum, average
+		long sum = Arrays.stream(numbers).sum();
+		double avg = Arrays.stream(numbers).average().getAsDouble();
+		System.out.println("\nsum=" + sum + " average=" + avg);
+		
+		//filter whose value greater than 5 and square it
+		Arrays.stream(numbers).filter(p -> p>5).map(p -> p*p).forEach(System.out::println);
 	}
 }
